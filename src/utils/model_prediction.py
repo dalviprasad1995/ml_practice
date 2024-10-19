b47815d8e6319ac2a04aeee4dbd8d8e307cab891
dalviprasad1995 <dalviprasad1995@gmail.com>
2024-10-20 00:16:29 +0530

    logging added

--- src/utils/model_prediction.py
+++ src/utils/model_prediction.py
@@ -11,20 +11,20 @@ os.chdir("E:/Prasad/Projects/ml_practice/")
 X_test = pd.read_csv("data/final/test/X_test.csv")
 y_test = pd.read_csv("data/final/test/y_test.csv")
 
-
-# Function to load the saved model
-def model_predict(X_test, y_test):
-    with open("model/model.pkl", "rb") as f:
-        model = pickle.load(f)
-
-    return model
+# Load the saved model
+with open("model/model.pkl", "rb") as f:
+    model_obj = pickle.load(f)
 
 
-# Load model on the train data
-model_obj = model_predict(X_test, y_test)
+# Function to predict on the test data using the saved model
+def model_predict(model_obj, X_test, y_test):
+    y_test["pred_prob"] = model_obj.predict(X_test)
+    return y_test
+
 
 # Predict on the test data using the saved model
-y_test["pred_prob"] = model_obj.predict(X_test)
+model_obj = model_predict(model_obj, X_test, y_test)
+
 
 # Write the files in the respective folder
 y_test.to_csv("results/y_test.csv", index=False)
